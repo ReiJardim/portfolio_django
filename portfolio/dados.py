4ded4e3875ae096b409cb1154a9bcdd9f71cfa8c
Rei WSL <reijardim.eng.civil@gmail.com>
2025-05-12 15:30:37 -0300

    Atualização - back-end finalizado

---
+++ portfolio/dados.py
@@ -0,0 +1,41 @@
+habilidades = {
+    'habilidade1': {
+        'nome': 'Python',
+    },
+    'habilidade2': {
+        'nome': 'Django',
+    },
+    'habilidade3': {
+        'nome': 'FLASK',
+    },
+
+}
+
+
+projetos = {
+    'projeto1': {
+        'titulo': 'Raspagem de dados',
+        'descricao': 'Raspagem de dados e extração de informações de sites utilizando a biblioteca BeautifulSoup e selenium',
+        'tecnologias': 'Python, Pandas, HTML, Selenium, BeautifulSoup',
+        'guithub': 'https://github.com/ReiJardim?tab=repositories'
+
+    },
+    'projeto2': {
+        'titulo': 'Mult agentes',
+        'descricao': 'Desenvolvimenrto de um sistema multiagente para execução de atividades de escrita',
+        'tecnologias': 'CREWAI, Python',
+        'guithub': 'https://github.com/ReiJardim/test_multagentes'
+    },
+    'projeto3': {
+        'titulo': 'Site de otmização de serviços',
+        'descricao': 'Desenvolvimento de site para otmizar serviços de geoprocessaamento e gerenciar obras',
+        'tecnologias': 'Python, streamlit, Pandas,',
+        'guithub': 'https://github.com/ReiJardim?tab=repositories'
+    },
+    'projeto4': {
+        'titulo': 'Sistema analise de Audios',
+        'descricao': 'Desenvolvimento de sistema para interação com audios, com funcionalidades de transcrição, tradução e gerar audio a partir de texto. Os projetos foram desenvolvidos com base em modelos do huggingface',
+        'tecnologias': 'Python, torch,transformers, soundfile, ...',
+        'guithub': 'https://github.com/ReiJardim/huggingface_audio'
+    },
+}
